6803f88522597a000c2e0cbf7fdfb8bda05a3920
Sid Vishnoi <sidvishnoi8@gmail.com>
2021-01-19 18:58:59 +0530

    add browser API

--- respec-config.js
+++ respec-config.js
@@ -6,4 +6,22 @@ var respecConfig = {
     branch: "main",
   },
   xref: "web-platform",
+  localBiblio: {
+    Interledger: {
+      title: "Interledger Protocol (ILP)",
+      href: "https://interledger.org/rfcs/0027-interledger-protocol-4/",
+    },
+    SPSP: {
+      title: "Simple Payments Setup Protocol",
+      href: "https://interledger.org/rfcs/0009-simple-payment-setup-protocol",
+    },
+    STREAM: {
+      title: "STREAM",
+      href: "https://interledger.org/rfcs/0029-stream/",
+    },
+    Receipt: {
+      title: "STREAM Receipt",
+      href: "https://interledger.org/rfcs/0039-stream-receipts/",
+    },
+  },
 };
