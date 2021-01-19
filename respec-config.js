d1e39e2680644b8a37993b14b84d374cc073d890
Sid Vishnoi <sidvishnoi8@gmail.com>
2021-01-19 18:58:28 +0530

    Initial commit

---
+++ respec-config.js
@@ -0,0 +1,9 @@
+var respecConfig = {
+  specStatus: "unofficial",
+  editors: [{ name: "Sid Vishnoi", url: "https://github.com/sidvishnoi" }],
+  github: {
+    repoURL: "https://github.com/sidvishnoi/webmonetization",
+    branch: "main",
+  },
+  xref: "web-platform",
+};
